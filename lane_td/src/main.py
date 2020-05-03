fdcca8acd4fcd79b4110fd9dc8309410ec9c8ca4
MicroOptimization <jackiezhen538@gmail.com>
2020-05-03 13:07:55 -0400

    started utilities module

--- lane_td/src/main.py
+++ lane_td/src/main.py
@@ -6,6 +6,7 @@ Github: github.com/MicroOptimization
 """
 import tkinter as tk
 from tkinter import Canvas
+import util
 
 root = tk.Tk()
 
@@ -14,30 +15,13 @@ window_height = 600
 
 canvas = Canvas(root, width=window_width, height=window_height)
 canvas.pack()
-"""
-#Draws 20 x 20px square
-canvas.create_line(20, 20, 20, 40)
-canvas.create_line(20, 40, 40, 40)
-canvas.create_line(40, 40, 40, 20)
-canvas.create_line(40, 20, 20, 20)
-"""
+root.resizable(False, False)
 
+#scale_coeff = 8
+#side_length = window_height / scale_coeff
+side_length = 75
 
-
-def draw_square(start_x, start_y, side_length, canvas):
-    canvas.create_line(start_x, start_y, start_x, start_y + side_length)
-    canvas.create_line(start_x, start_y + side_length, start_x + side_length, start_y + side_length)
-    canvas.create_line(start_x + side_length, start_y + side_length, start_x + side_length, start_y)
-    canvas.create_line(start_x + side_length, start_y, start_x, start_y)
-
-scale_coeff = 8
-
-side_length = window_height / scale_coeff
-start_x = 10
-start_y = 10
-
-#draw_square(start_x, start_y, side_length, canvas)
-
+print(side_length)
 grid_height = 6
 grid_width = 9
 
@@ -47,9 +31,35 @@ for j in range(grid_width):
         x1 = 10 + (side_length * j)
         y1 = 80 + (side_length * i)
         
-        draw_square(x1, y1, side_length, canvas)
+        util.draw_square(x1, y1, side_length, canvas)
 
 
 while True:
     root.update_idletasks()
     root.update()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
